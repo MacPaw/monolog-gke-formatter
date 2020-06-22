c9ca28367577d1454f3f4615d17da52974b8039a
Andrii Nasinnyk <anasinnyk@macpaw.com>
2020-06-23 01:49:12 +0300

    feat: http request data
    Signed-off-by: Andrii Nasinnyk <anasinnyk@macpaw.com>

--- src/GkeFormatter.php
+++ src/GkeFormatter.php
@@ -2,20 +2,23 @@
 
 namespace MacPaw\MonologGkeFormatter;
 
+use GuzzleHttp\Psr7\ServerRequest;
 use Monolog\Formatter\JsonFormatter;
 
 class GkeFormatter extends JsonFormatter
 {
     public function format(array $record): string
     {
+        $request = ServerRequest::fromGlobals();
+
         return parent::format(
             array_merge(
                 $record['extra'],
-                isset($record['exception'])
+                isset($record['context']) && isset($record['context']['exception'])
                     ? [
                         'sourceLocation' => [
-                            'file' => str_split(':', $record['exception']['file'])[0],
-                            'line' => str_split(':', $record['exception']['file'])[1]
+                            'file' => str_split(':', $record['context']['exception']['file'])[0],
+                            'line' => str_split(':', $record['context']['exception']['file'])[1]
                         ]
                     ]
                     : [],
@@ -25,6 +28,16 @@ class GkeFormatter extends JsonFormatter
                     'severity' => $record['level_name'],
                     'serviceContext' => $record['context'],
                     'timestamp' => $record['datetime']->getTimestamp(),
+                    'httpRequest' => [
+                        'requestMethod' => $request->getMethod(),
+                        'requestUrl' => $request->getUri()->__toString(),
+                        'requestSize' => $request->getBody()->getSize(),
+                        'protocol' => $request->getProtocolVersion(),
+                        'referer' => $request->getHeader('Referer'),
+                        'userAgent' => $request->getHeader('User-Agent'),
+                        'remoteIp' => $request->getHeader('X-Forwarded-For'),
+                        'serverIp' => $_SERVER['SERVER_ADDR'],
+                    ],
                 ]
             )
         );
