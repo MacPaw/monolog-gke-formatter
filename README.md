dce5411eea88a121f6d0ade2ce8bb68e77386007
Andrii Nasinnyk <anasinnyk@macpaw.com>
2020-06-22 14:54:47 +0300

    feat: gke monolog formatter
    Signed-off-by: Andrii Nasinnyk <anasinnyk@macpaw.com>

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# monolog-gke-formatter
-Monolog GKE formatter
+# Monolog extension for Google Cloud logging formatter
+
+This library can re-format json log to Google Kubernetes Engine format 
+
+## Installation
+
+```
+composer require macpaw/monolog-gke-formatter
+```
+
+## Usage
+
+```php
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use MacPaw\MonologGkeFormatter\GkeFormatter;
+
+$handler = new StreamHandler('php://stdout');
+$handler->setFormatter(new GkeFormatter());
+```
---
+++ src/GkeFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace MacPaw\MonologGkeFormatter;
+
+use Monolog\Formatter\JsonFormatter;
+
+class GkeFormatter extends JsonFormatter
+{
+    public function format(array $record): string
+    {
+        return parent::format(
+            array_merge(
+                $record['extra'],
+                isset($record['file'])
+                    ? [
+                        'sourceLocation' => [
+                            'file' => str_split(':', $record['file'])[0],
+                            'line' => str_split(':', $record['file'])[1]
+                        ]
+                    ]
+                    : [],
+                [
+                    'message' => $record['message'],
+                    'thread' => $record['channel'],
+                    'severity' => $record['level_name'],
+                    'serviceContext' => $record['context'],
+                    'timestamp' => $record['datatime']->getTimestamp(),
+                ]
+            )
+        );
+    }
+}
