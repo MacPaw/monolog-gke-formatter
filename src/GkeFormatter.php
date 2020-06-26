d6f2dd3653c901558bb096d0b073a03edd5f2f06
Andrii Nasinnyk <anasinnyk@macpaw.com>
2020-06-26 18:27:18 +0300

    feat: add request and source location
    Signed-off-by: Andrii Nasinnyk <anasinnyk@macpaw.com>

--- src/GkeFormatter.php
+++ src/GkeFormatter.php
@@ -10,24 +10,28 @@ class GkeFormatter extends JsonFormatter
     public function format(array $record): string
     {
         $request = ServerRequest::fromGlobals();
+        $debug = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,5);
 
         return parent::format(
             array_merge(
                 $record['extra'],
-                isset($record['context']) && isset($record['context']['exception'])
+                isset($debug[3])
                     ? [
-                        'sourceLocation' => [
-                            'file' => str_split(':', $record['context']['exception']['file'])[0],
-                            'line' => str_split(':', $record['context']['exception']['file'])[1]
-                        ]
+                    'sourceLocation' => [
+                        'file' => $debug[3]['file'],
+                        'line' => $debug[3]['line'],
+                        'function' => isset($debug[4]) && isset($debug[4]['class']) && isset($debug[4]['function'])
+                            ? sprintf("%s:%s", $debug[4]['class'], $debug[4]['function'])
+                            : (
+                            isset($debug[4]) && isset($debug[4]['function'])
+                                ? $debug[4]['function']
+                                : ''
+                            ),
                     ]
+                ]
                     : [],
-                [
-                    'message' => $record['message'],
-                    'thread' => $record['channel'],
-                    'severity' => $record['level_name'],
-                    'serviceContext' => $record['context'],
-                    'timestamp' => $record['datetime']->getTimestamp(),
+                preg_match('/cgi/', php_sapi_name())
+                    ? [
                     'httpRequest' => [
                         'requestMethod' => $request->getMethod(),
                         'requestUrl' => $request->getUri()->__toString(),
@@ -37,6 +41,14 @@ class GkeFormatter extends JsonFormatter
                         'userAgent' => $request->getHeaderLine('User-Agent'),
                         'remoteIp' => $request->getHeaderLine('X-Forwarded-For'),
                     ],
+                ]
+                    : [],
+                [
+                    'message' => $record['message'],
+                    'thread' => $record['channel'],
+                    'severity' => $record['level_name'],
+                    'serviceContext' => $record['context'],
+                    'timestamp' => $record['datetime']->getTimestamp(),
                 ]
             )
         );
