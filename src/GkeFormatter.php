0f481a355539d38ac3e1d3032681247b58d51237
Andrii Nasinnyk <anasinnyk@macpaw.com>
2020-07-02 15:02:22 +0300

    feat: optionaly include context
    Signed-off-by: Andrii Nasinnyk <anasinnyk@macpaw.com>

--- src/GkeFormatter.php
+++ src/GkeFormatter.php
@@ -7,41 +7,61 @@ use Monolog\Formatter\JsonFormatter;
 
 class GkeFormatter extends JsonFormatter
 {
+    const BACKTRACE_DEFAULT_CALL = 6;
+
+    protected $deepToBacktrace;
+    protected $httpRequestContext;
+    protected $sourceLocationContext;
+
+    public function __construct(
+        int $batchMode = self::BATCH_MODE_JSON,
+        bool $appendNewline = true,
+        bool $ignoreEmptyContextAndExtra = false,
+        bool $httpRequestContext = false,
+        bool $sourceLocationContext = false,
+        int $deepToBacktrace = self::BACKTRACE_DEFAULT_CALL
+    ) {
+        parent::__construct($batchMode, $appendNewline, $ignoreEmptyContextAndExtra);
+        $this->httpRequestContext = $httpRequestContext;
+        $this->sourceLocationContext = $sourceLocationContext;
+        $this->deepToBacktrace = $deepToBacktrace;
+    }
+
     public function format(array $record): string
     {
         $request = ServerRequest::fromGlobals();
-        $debug = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,6);
+        $debug = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS,$this->deepToBacktrace);
 
         return parent::format(
             array_merge(
                 $record['extra'],
-                isset($debug[4])
+                $this->sourceLocationContext && isset($debug[$this->deepToBacktrace-2])
                     ? [
-                    'sourceLocation' => [
-                        'file' => $debug[4]['file'],
-                        'line' => $debug[4]['line'],
-                        'function' => isset($debug[5]) && isset($debug[5]['class']) && isset($debug[5]['function'])
-                            ? $debug[5]['class'] . $debug[5]['type'] . $debug[5]['function']
-                            : (
-                            isset($debug[5]) && isset($debug[5]['function'])
-                                ? $debug[5]['function']
-                                : ''
-                            ),
+                        'sourceLocation' => [
+                            'file' => $debug[$this->deepToBacktrace-2]['file'],
+                            'line' => $debug[$this->deepToBacktrace-2]['line'],
+                            'function' => isset($debug[$this->deepToBacktrace-1]) && isset($debug[$this->deepToBacktrace-1]['class']) && isset($debug[$this->deepToBacktrace-1]['function'])
+                                ? $debug[$this->deepToBacktrace-1]['class'] . $debug[$this->deepToBacktrace-1]['type'] . $debug[$this->deepToBacktrace-1]['function']
+                                : (
+                                isset($debug[$this->deepToBacktrace-1]) && isset($debug[$this->deepToBacktrace-1]['function'])
+                                    ? $debug[$this->deepToBacktrace-1]['function']
+                                    : ''
+                                ),
+                        ]
                     ]
-                ]
                     : [],
-                preg_match('/cgi/', php_sapi_name())
+                $this->httpRequestContext && preg_match('/cgi/', php_sapi_name())
                     ? [
-                    'httpRequest' => [
-                        'requestMethod' => $request->getMethod(),
-                        'requestUrl' => $request->getUri()->__toString(),
-                        'requestSize' => $request->getBody()->getSize(),
-                        'protocol' => $request->getProtocolVersion(),
-                        'referer' => $request->getHeaderLine('Referer'),
-                        'userAgent' => $request->getHeaderLine('User-Agent'),
-                        'remoteIp' => $request->getHeaderLine('X-Forwarded-For'),
-                    ],
-                ]
+                        'httpRequest' => [
+                            'requestMethod' => $request->getMethod(),
+                            'requestUrl' => $request->getUri()->__toString(),
+                            'requestSize' => $request->getBody()->getSize(),
+                            'protocol' => $request->getProtocolVersion(),
+                            'referer' => $request->getHeaderLine('Referer'),
+                            'userAgent' => $request->getHeaderLine('User-Agent'),
+                            'remoteIp' => $request->getHeaderLine('X-Forwarded-For'),
+                        ],
+                    ]
                     : [],
                 [
                     'message' => $record['message'],
